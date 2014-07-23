e7cbcd5914d43e8c406c7755f6e9d02d0f731e95
jian-wei <jw.chuah@utoronto.ca>
2014-07-23 17:14:12 +0800

    Updated images, css and front page content

---
+++ js/background.js
@@ -0,0 +1,26 @@
+var tmp = '/Users/jchuah/Github/jianweichuah.github.io/'
+
+$(function() {
+	$.vegas('slideshow', {
+		backgrounds:[
+			{ src: tmp + 'images/1.jpg', fade: 2000},
+			{ src: tmp + 'images/2.jpg', fade: 2000},
+			{ src: tmp + 'images/3.jpg', fade: 2000},
+			{ src: tmp + 'images/4.jpg', fade: 2000},
+			{ src: tmp + 'images/5.jpg', fade: 2000},
+			{ src: tmp + 'images/6.jpg', fade: 2000},
+			{ src: tmp + 'images/7.jpg', fade: 2000},
+			{ src: tmp + 'images/8.jpg', fade: 2000},
+			{ src: tmp + 'images/9.jpg', fade: 2000},
+			{ src: tmp + 'images/10.jpg', fade: 2000},
+			{ src: tmp + 'images/11.jpg', fade: 2000}
+		]
+	});
+	$.vegas('overlay', {
+		src:'/Users/jchuah/Github/jianweichuah.github.io/js/vegas/overlays/01.png'
+	});
+});
+$(document).ready(function() { 
+	$(".content").css("display", "none");
+	$(".content").fadeIn(2000);
+});
--- js/main.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-	$.vegas({
-		src:'http://sknny.ru/img/background.jpg'
-	});
-	$.vegas('overlay', {
-		src:'/js/vegas/overlays/13.png'
-	});
-});
