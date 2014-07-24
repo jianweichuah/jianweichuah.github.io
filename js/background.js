4bfa70181b99cf4c59c02eefa0ccba3109169703
jian-wei <jw.chuah@utoronto.ca>
2014-07-24 11:49:26 +0800

    got rid of debugging strings

--- js/background.js
+++ js/background.js
@@ -1,19 +1,17 @@
-var tmp = ''
-
 $(function() {
 	$.vegas('slideshow', {
 		backgrounds: [
-			{ src: tmp + 'images/1.jpg', fade: 2000},
-			{ src: tmp + 'images/2.jpg', fade: 2000},
-			{ src: tmp + 'images/3.jpg', fade: 2000},
-			{ src: tmp + 'images/4.jpg', fade: 2000},
-			{ src: tmp + 'images/5.jpg', fade: 2000},
-			{ src: tmp + 'images/6.jpg', fade: 2000},
-			{ src: tmp + 'images/7.jpg', fade: 2000},
-			{ src: tmp + 'images/8.jpg', fade: 2000},
-			{ src: tmp + 'images/9.jpg', fade: 2000},
-			{ src: tmp + 'images/10.jpg', fade: 2000},
-			{ src: tmp + 'images/11.jpg', fade: 2000}
+			{ src: 'images/1.jpg', fade: 2000},
+			{ src: 'images/2.jpg', fade: 2000},
+			{ src: 'images/3.jpg', fade: 2000},
+			{ src: 'images/4.jpg', fade: 2000},
+			{ src: 'images/5.jpg', fade: 2000},
+			{ src: 'images/6.jpg', fade: 2000},
+			{ src: 'images/7.jpg', fade: 2000},
+			{ src: 'images/8.jpg', fade: 2000},
+			{ src: 'images/9.jpg', fade: 2000},
+			{ src: 'images/10.jpg', fade: 2000},
+			{ src: 'images/11.jpg', fade: 2000}
 		],
 		preload: true
 	});
