d2195c394af76a7997f61cf694d1e10e30774e4b
jianweichuah <jw.chuah@utoronto.ca>
2015-03-03 02:16:26 -0500

    Implemented lazy loading and cleaned up the page

--- projects/miniyoutube/js/miniyoutube.js
+++ projects/miniyoutube/js/miniyoutube.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-    NProgress.start();
-    $(".features").css("display", "none");
-});
-
-$(window).load(function() {
-    NProgress.done();
-    $(".features").fadeIn(1500);
+    $("img.img-polaroid").lazyload({
+        effect: "fadeIn"
+    });
 });
