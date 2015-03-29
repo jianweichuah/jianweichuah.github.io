f20f47e112f80b09af6b06c6291f86813f1a50ea
jianweichuah <jw.chuah@utoronto.ca>
2015-03-29 18:25:28 -0400

    Changed background to particleJS

--- projects/miniyoutube/js/miniyoutube.js
+++ projects/miniyoutube/js/miniyoutube.js
@@ -2,4 +2,7 @@ $(document).ready(function() {
     $("img.img-polaroid").lazyload({
         effect: "fadeIn"
     });
+
+    var numActiveUsersAnimation = new countUp("number-of-users", 0, 2741);
+    numActiveUsersAnimation.start();
 });
