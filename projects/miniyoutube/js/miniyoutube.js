b5b9bbdc8bda8001b9e91811b0867bdc1a5c473e
jianweichuah <jw.chuah@utoronto.ca>
2015-07-27 22:17:56 -0400

    Minor updates to mini youtube page

--- projects/miniyoutube/js/miniyoutube.js
+++ projects/miniyoutube/js/miniyoutube.js
@@ -1,8 +1,3 @@
 $(document).ready(function() {
-    $("img.img-polaroid").lazyload({
-        effect: "fadeIn"
-    });
-
-    var numActiveUsersAnimation = new countUp("number-of-users", 0, 2741);
-    numActiveUsersAnimation.start();
+    $("img.img-polaroid").lazyload();
 });
